3ac7791f18baf188b8d3fe0f5fb41b84b67abac3
Kecein <fyt73747@gmail.com>
2023-09-12 18:44:40 +0300

    add new functions

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+using namespace std;;
 int main()
-{
-
+{	
+	setlocale(LC_ALL, "");
+	int a;
+	int b;
+	cout << "Введите первое число:" << endl;
+	cin >> a;
+	cout << "Введите второе число:" << endl;
+	cin >> b;
+	cout << "Ответ: " << a + b;
 }
